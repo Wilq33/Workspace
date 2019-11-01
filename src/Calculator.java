2bf0dd64fb000d2c25e34fde14c5b96b7d830700
Bartlomiej Wilk <34482885+Wilq33@users.noreply.github.com>
2019-11-01 16:04:58 +0100

    First commit
    First commit

---
+++ src/Calculator.java
@@ -0,0 +1,57 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Calculator {
+
+    public Calculator(){
+
+      System.out.println("Calling constructor");
+
+
+    }
+
+
+    public void add() {
+
+      System.out.println("Adding numbers");
+
+    }
+
+    public void sub() {
+
+        System.out.println("Subtract numbers");
+
+    }
+
+    public void div() {
+
+        System.out.println("Divide numbers");
+
+    }
+
+    public void mult() {
+
+        System.out.println("Multiply numbers");
+
+    }
+
+
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+        calc.add();
+        calc.div();
+        calc.sub();
+        calc.mult();
+
+        new Calculator().add();
+        new Calculator().sub();
+        new Calculator().div();
+        new Calculator().mult();
+
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.get("http://way2automation.com");
+
+    }
+
+}
